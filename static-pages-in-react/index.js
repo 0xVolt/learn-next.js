de7d2ca59525592163ce0092807f9bef5739b47f
Desh Iyer <desh.iyer@gmail.com>
2023-04-01 16:30:11 +0530

    Update index.js

--- static-pages-in-react/index.js
+++ static-pages-in-react/index.js
@@ -1,2 +1,2 @@
 // Here's my first line of React code. It's to render some text to the page. We use the global variable ReactDOM to render text that seems to be in a similar format to html.
-ReactDOM.render(<h1>Mozambique 'ere!</h1>, document.getElementById('root'))
+ReactDOM.render(<h1>SURBHI SIMULATOR</h1>, document.getElementById('root'))
