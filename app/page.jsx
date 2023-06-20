16cd0feab7aa67c7309598d14723fee20fb3af7b
Desh Iyer <desh.iyer@gmail.com>
2023-06-20 18:18:41 +0400

    🔨 Create heading and description

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,22 @@
 const Home = () => {
   return (
-    <div>
-      Home page.
-    </div>
+    // Creating a section in html for the title
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover & Share
+
+        {/* To create a gap for smaller screen (md) */}
+        <br className='max-md:hidden' />
+
+        <span className='orange_gradient text-center'> AI Powered Prompts</span>
+      </h1>
+
+      <p className='desc text-center'>
+        This is a test website that's built in Next.js, an extension of React that offers features like server-side rendering and more.
+      </p>
+    </section>
+
+    // Here's where the feed component comes in
   )
 }
 
