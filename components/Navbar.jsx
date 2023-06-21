f7c6420e788054ddb1cc6ce34fa124b2368c57b8
Desh Iyer <desh.iyer@gmail.com>
2023-06-21 16:07:25 +0400

    🔨 Update Navbar component design for desktop devices

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+'use client';
+
+// To enable links to other pages
+import Link from 'next/link';
+// To optimise images on this page
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+// For easy login functionality
+import { signIn, signOut, getProviders, useSession } from 'next-auth/react';
 
 const Navbar = () => {
+  const userLoggedIn = true;
+
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex-between mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
+        <Image 
+          src='/assets/images/logo.svg'
+          alt='Logo'
+          width={30}
+          height={30}
+          className='object-contain'
+        />
+
+        {/* <p className='logo_text'>Test Website</p> */}
+      </Link>
+
+      {/* Desktop navigation now. */}
+      <div className='sm:flex hidden'>
+        {userLoggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link 
+              href='/create-post' 
+              className='black_btn'
+            >
+              Create Post
+            </Link>
+
+            <button type='button' onClick={ signOut } className='outline_btn'>
+              Sign Out
+            </button>
+          </div>
+        ) : (
+          <>
+          
+          </>
+        )}
+      </div>
+    </nav>
   )
 }
 
